2304f0c8aae6d061248a4e8a4021fa70de39fd19
Allyson <allyson@teste.com>
2015-10-03 15:53:45 -0300

    Fim server.js

--- aula-03-10/http/server.js
+++ aula-03-10/http/server.js
@@ -1,7 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function (req, res) {//req = request res = response
   	console.log("URL:",req.url);
+
+	fs.readFile("request.log",'utf8',function(err,data){
+		if (err) throw err;
+		var appendedData = data + '\n' + req.url
+		fs.writeFile("request.log",appendedData,function(err){
+			if (err) throw err;
+		});
+	});
+
+
 	res.writeHead(200, {'Content-Type': 'text/plain'});
  	res.end('Hello World\n');
 });
