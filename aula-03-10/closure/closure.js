c9046184662e43e257b65782878a699437993a7c
Allyson <allyson@teste.com>
2015-10-03 12:12:06 -0300

    Que legal

--- aula-03-10/closure/closure.js
+++ aula-03-10/closure/closure.js
@@ -3,8 +3,11 @@ function sayHellow(name){
 	console.log("Hi " + name);
 }
 
+
+function executor(callback,name){
+	callback(name || "no name");
+}
+
 sayHellow("Teste");
-
-
-
+executor(sayHellow,"Allyson");
 
