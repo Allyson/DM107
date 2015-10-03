6944bda254efa20bd80c763e96be68c4236d59d5
Allyson <allyson@teste.com>
2015-10-03 17:28:05 -0300

    Fim

---
+++ aula-03-10/todo-nodejs-app/server.js
@@ -0,0 +1,15 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+// Express configuration
+var app = express();
+app.use(bodyParser.json());
+//welcome
+app.get('/', function(req, res) {
+res.send('TODO List REST API');
+});
+// Task API
+app.use('/api/tasks', require('./app/taskApi'));
+var server = app.listen(8080, function() {
+console.log("Server running at http://localhost:8080");
+});
