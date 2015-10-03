12e65c86e286c5923f937cadb29dc8aaf3908384
Allyson <allyson@teste.com>
2015-10-03 11:58:14 -0300

    Primeiro exemplo

---
+++ aula-03-10/helloworld/helloworld.js
@@ -0,0 +1,20 @@
+
+
+
+//Vetor
+[
+	'Teste',
+	'Teste1',
+	'Teste2',
+	'Teste3',
+	'Teste4',
+	'Teste5',
+	'Teste6',
+	'Teste7',
+	'Teste8'
+].forEach(function(name){
+	console.log("Olá, "+ name);
+})
+
+
+
